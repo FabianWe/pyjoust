5202412c0963a64e557fd6d8baae525aaf9005c3
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2018-11-08 18:11:16 +0100

    encoding and stuff, soon to change though

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Copyright 2018 Fabian Wenzelmann <fabianwen@posteo.eu>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,21 +14,39 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pyjoust.group import round_robin, TwoPointsTable, ThreePointsTable
+from pyjoust.group import round_robin, TwoPointsTable
 from pyjoust.ko import KOTree
-from pyjoust.utils import GoalScore
 from pyjoust.kubb import KubbResult
+from pyjoust.utils import GoalScore
+
+import pygubu
+import tkinter as tk
+
+# if __name__ == '__main__':
+#     p = list(range(15))
+#     matches = round_robin(p)
+#     t = TwoPointsTable(p, matches)
+#     t.set_match_from_string(0, 1, "42:24")
+#     tree = KOTree(list(range(16)))
+#     rows = tree.get_rows()
+#     print(list(tree.get_matches(rows[0])))
+#     print(GoalScore.parse("42:24"))
+#
+#     f = getattr(KubbResult, 'compute_ranks', None)
+#     print(f)
+#     print(callable(f))
+
+class App(object):
+    def __init__(self, master):
+        self.builder = builder = pygubu.Builder()
+        builder.add_from_file('pyjoust/wizard_welcome.ui')
+        self.mainwindow = builder.get_object('team_frame', master)
+        builder.connect_callbacks(self)
+
+    def next(self, event=None):
+        print(event)
 
 if __name__ == '__main__':
-    p = list(range(15))
-    matches = round_robin(p)
-    t = TwoPointsTable(p, matches)
-    t.set_match_from_string(0, 1, "42:24")
-    tree = KOTree(list(range(16)))
-    rows = tree.get_rows()
-    print(list(tree.get_matches(rows[0])))
-    print(GoalScore.parse("42:24"))
-
-    f = getattr(KubbResult, 'compute_ranks', None)
-    print(f)
-    print(callable(f))
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
--- pyjoust/ko.py
+++ pyjoust/ko.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Copyright 2018 Fabian Wenzelmann <fabianwen@posteo.eu>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
